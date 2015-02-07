d48ea1cae750b1e769d6a09629d2ddecd4a8bb00
Daniel Au <dakmau@gmail.com>
2015-02-07 06:02:15 -0500

    Move Tree complete with bugs

---
+++ knight_searcher.rb
@@ -0,0 +1,18 @@
+class KnightSearcher
+  def initialize (tree)
+    @tree = tree
+  end
+
+  def bfs_for (target_coords)
+    # Perform a breadth-first search
+    # Ensure squares aren't hit twice by checking depth attribute
+    # After locating the target square, output the sequence of moves
+  end
+
+  def dfs_for (target_coords)
+    # Perform a breadth-first search
+    # Ensure squares aren't hit twice by checking depth attribute
+    # After locating the target square, output the sequence of moves
+  end
+
+end
