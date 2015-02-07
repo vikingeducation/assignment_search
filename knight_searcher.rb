c73e9dbbba8b2d593419f69f4fdd47628a7f3c47
Dan Berger <dsberger@gmail.com>
2015-02-06 18:34:29 -0800

    collapsed bfs and dfs into search method with passed blocks

--- knight_searcher.rb
+++ knight_searcher.rb
@@ -10,28 +10,24 @@ class KnightSearcher
     @tree = tree
   end
 
-  def bfs_for(target_coords)
-    search_queue = []
+  def search(target_coords)
+    to_be_searched = []
     node = tree.root
     until [node.x,node.y] == target_coords
-      search_queue += node.children
-      node = search_queue.shift
+      to_be_searched += node.children
+      node = yield(to_be_searched)
     end
     puts "#{target_coords} was found in #{node.depth} moves."
     print_results(node)
     return node.depth
   end
 
+  def bfs_for(target_coords)
+    search(target_coords) { |queue| queue.shift }
+  end
+
   def dfs_for(target_coords)
-    search_stack = []
-    node = tree.root
-    until [node.x,node.y] == target_coords
-      search_stack += node.children
-      node = search_stack.pop
-    end
-    puts "#{target_coords} was found in #{node.depth} moves."
-    print_results(node)
-    return node.depth
+    search(target_coords) { |stack| stack.pop }
   end
 
   def print_results(node)
