d4aae623548c400f1cf247a46b4701cc5d39c2cc
Daniel Au <dakmau@gmail.com>
2015-02-07 06:05:19 -0500

    Psuedo code for knightsearcher

--- knight_searcher.rb
+++ knight_searcher.rb
@@ -10,9 +10,15 @@ class KnightSearcher
   end
 
   def dfs_for (target_coords)
-    # Perform a breadth-first search
+    # Perform a depth-first search
     # Ensure squares aren't hit twice by checking depth attribute
     # After locating the target square, output the sequence of moves
   end
 
+  def scout_node(node, target_coords)
+    # Receives a node
+    # Checks every one of the child node
+    # returns the child nodes if it matches the target coordinates
+    # else it quits
+  end
 end
