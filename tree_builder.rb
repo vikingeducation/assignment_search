52cde3c2818cea0faa0930142413ce5fd4eec224
Alice Fung <alice.o.fung@gmail.com>
2015-07-08 19:56:46 -0400

    create tree method

--- tree_builder.rb
+++ tree_builder.rb
@@ -1,32 +1,35 @@
 Square = Struct.new(:x, :y, :depth, :children) #children = array of squares
 
 class MoveTree
-  attr_reader :squares
+  attr_reader :squares, :root
+  #attr_writer :children
 
-  def initialize(coord = [0,0], max_depth = 1) #constructs a tree
+  def initialize(coord = [0,0], max_depth = 2) #constructs a tree
     @squares = []
+    @nodes = 0
     @origin = coord
+    @depth = 0
     @board = create_board
     @root = create_root
-    @depth = 0
+    @max_depth = 2
     #create_tree(depth) #loop
   end
 
   def create_root # no children yet
+    @nodes +=1
+    #@depth +=1
     Square.new(@origin[0], @origin[1], 0)
   end
 
-  def create_tree(parent, depth)
-    level = 0
-    parent = @root
-    until level == depth
-      level +=1
-      children.each do |child|
-        Square.new(child.x, child.y, level, potential_moves(child[0], child[1], level)
-      end
-    children = @root.children
+  def create_tree(level, parent)
+    return if level > @max_depth
+    parent.children = potential_moves([parent.x, parent.y], level)
+    parent.children.each do |child|
+      create_tree(level+1, parent)
+      # child.children = potential_moves([child.x, child.y], level)
+      # Square.new(child.x, child.y, level, potential_moves(child[0], child[1], level))
     end
-
+      # parent.children.each {|c| queue << c} # ARRAY, queue
   end
 
   def create_board #creates 8x8 board
@@ -40,8 +43,9 @@ class MoveTree
     moves = [[1,2], [1,-2], [-1,2], [-1,-2], [2,1], [2,-1], [-2,-1], [-2,1]]
     squares = []
     moves.each do |move|
-      puts temp = [@coord[0] + move[0],@coord[1]+move[1]]
+      temp = [origin[0] + move[0], origin[1]+move[1]]
       if valid_coord(temp)
+        @nodes += 1
         squares << Square.new(temp[0],temp[1], level)
       end
     end
@@ -49,7 +53,7 @@ class MoveTree
   end
 
   def inspect
-    puts" Your tree has #{@squares.length + 1} nodes and a maximum depth of #{@max_depth}"
+    puts" Your tree has #{@nodes} nodes and a maximum depth of #{@depth}"
   end
 
 
