f3bb4994a398d04172a72a4eec88aa9cb6660d38
michelletoussaint <toussaint.michelle@gmail.com>
2015-07-08 18:47:31 -0400

    Fixing depth recursive loop

--- knight.rb
+++ knight.rb
@@ -2,20 +2,28 @@ Square = Struct.new(:x,:y,:depth, :children)
 
 class MoveTree 
 
-  def initialize(init_x = 0, init_y = 0, max_depth = 1)    #board top left corner
+  attr_reader :head
+
+  def initialize(init_x = 0, init_y = 0, max_depth = 1)
 
     @max_depth = max_depth
     @depth = 0
-    @head = Square(init_x, init_y, depth, [])
+    @head = Square.new(init_x, init_y, @depth, [])
     construct_nodes(@head)
 
   end
 
   def construct_nodes(parent)
-    @depth +=1
-    if @depth <= @max_depth
-      create_moves(parent)
-      create_children(valid_moves, parent)
+
+    while @depth < @max_depth
+      @depth +=1
+      valid_moves = create_moves(parent)
+      new_parent_array = create_children(valid_moves, parent)
+
+      #recursion
+      new_parent_array.each do|new_parent| construct_nodes(new_parent)
+      end
+
     end
 
   end
@@ -24,11 +32,12 @@ class MoveTree
     moves_arr = []
     [-2,-1,1,2].each do |x|
       [-2,-1,1,2].each do |y|
-        if abs(x + y)==3
+        if (x.abs + y.abs) == 3
           moves_arr << [square.x+x, square.y+y]
         end
       end
     end
+      # puts moves_arr.inspect
       validate_legal_moves(moves_arr)
   end
 
@@ -39,6 +48,7 @@ class MoveTree
           valid_moves << move
       end
     end
+    puts valid_moves.inspect
     valid_moves
   end
 
@@ -46,10 +56,22 @@ class MoveTree
     valid_moves.each do |move|
       parent.children << Square.new(move[0], move[1], @depth, [])
     end
+    parent.children #[sq1, sq2]
   end
 
-
   def inspect
+
+    print @head.inspect
+
+    @depth.times do
+     #  @head +
+     #  @head.children.lenght
+     #  each children 
+     #   children.lenght
+     # end
+
+
+    end
 
 
   end
