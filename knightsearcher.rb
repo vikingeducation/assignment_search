e847886eae94019f6e3a0dd94f602fc806b9bd8a
Alice Fung <alice.o.fung@gmail.com>
2015-07-08 23:10:21 -0400

    fix method before testing

--- knightsearcher.rb
+++ knightsearcher.rb
@@ -6,18 +6,40 @@ class KnightSearcher
     @tree = tree
   end
 
-  def bfs_for (goal) #incomplete
+  def bfs_for(goal)
     origin = @tree.root
     queue = []
     current_node = [origin.x, origin.y]
     until [current_node.x, current_node.y] == goal
       current_node.children.each do |child|
-        queue += queue_children(child)
+        queue << child
       end
       current_node = queue.shift
     end
+    find_path(current_node)
   end
 
+  def dfs_for(goal)
+    origin = @tree.root
+    stack = []
+    current_node = [origin.x, origin.y]
+    until [current_node.x, current_node.y] == goal
+      current_node.children.each do |child|
+        stack << child
+      end
+      current_node = stack.pop
+    end
+    find_path(current_node)
+  end
+
+  def find_path(node)
+    path = [[node.x, node.y]]
+    until node.parent == @tree.root
+      node = node.parent
+      path << [node.x, node.y]
+    end
+    path.reverse
+  end
 
   def queue_children(parent)
     append_queue = []
