dfcd5cdf2bdd2b4dabd4e7b7bb185a022dc75407
Justin Mullis <justin.mullis@gmail.com>
2015-08-12 20:59:30 -0400

    build tree hard coded depth of 1

--- tree_builder.rb
+++ tree_builder.rb
@@ -1,22 +1,24 @@
-square = Struct.new(:x, :y, :depth, :children)
+Square = Struct.new(:x, :y, :depth, :children)
 
 class MoveTree
 
   def initialize(start, max_depth)
-    @start = start
+    @root = Square.new(start[0], start[1], 0, [])
     @max_depth = max_depth
   end
 
 
   def build_tree
-    
+    moves = get_valid_moves_from(@root.x, @root.y)
+    moves.each do |move|
+      child = Square.new(move[0], move[1], 1, [])
+      @root.children << child
+    end    
   end
   
 
-  def get_valid_moves_from(coords)
+  def get_valid_moves_from(x, y)
 
-    x = coords[0]
-    y = coords[1]
     valid_moves = []
     legal_moves = [ [x+1, y-2], [x+2, y-1], [x+2, y+1], [x+1, y+2],
                     [x-1, y+2], [x-2, y+1], [x-2, y-1], [x-1, y-2] ]
