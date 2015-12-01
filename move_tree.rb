1f5b77ca3a2c335f4d94e9b4e6635ff55b3cf873
Sia <siakaramalegos@gmail.com>
2015-11-30 20:22:57 -0600

    Adds ancestor tracking for easier output in knight searcher

--- move_tree.rb
+++ move_tree.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-Square = Struct.new(:x, :y, :depth, :children)
+Square = Struct.new(:x, :y, :depth, :children, :ancestors)
 
 # Tree of potential moves based on a starting position and maximum number of moves (depth)
 class MoveTree
@@ -9,7 +9,7 @@ class MoveTree
   def initialize(start_coords, max_depth = 1, board_size = 5)
     @board_size = board_size
     @max_depth = max_depth
-    @head = Square.new(start_coords[0], start_coords[1], 0)
+    @head = Square.new(start_coords[0], start_coords[1], 0, nil, [])
     @node_count = 1
     get_moves(@head)
   end
@@ -26,8 +26,16 @@ class MoveTree
       moves = get_potential_moves(node)
       moves.each do |move|
         @node_count += 1
-        child = Square.new(move[0], move[1], node.depth + 1)
+
+        # Create child node
+        child = Square.new(move[0], move[1], node.depth + 1, nil, [])
+        node.ancestors.each{ |ancestor| child.ancestors << ancestor }
+        child.ancestors << [node.x, node.y]
+
+        # Put child node into children of current node
         node.children << child
+
+        # Recursively build next depth of children
         get_moves(child)
       end
     end
@@ -54,7 +62,7 @@ class MoveTree
   end
 end
 
-tree = MoveTree.new([3,3], 1)
-tree.inspect
-new_tree = MoveTree.new([2,2], 3)
-new_tree.inspect
+# tree = MoveTree.new([3,3], 1)
+# tree.inspect
+# new_tree = MoveTree.new([2,2], 2)
+# new_tree.inspect
