ac74e1331405756f7a0e04f368920b73789c2c65
Kelsey James <kelseybjames@gmail.com>
2016-01-13 14:15:38 -0800

    Started pseudocode

---
+++ pseudocode.rb
@@ -0,0 +1,12 @@
+# Searching a simple tree of nodes where each Node has an array of child nodes (some_node.children) using DFS.
+
+def dfs_search(stack, item, tree)
+  return item if tree.root == item
+  
+end
+
+# Searching the same tree using BFS.
+
+# Searching a graph (represented however you feel most comfortable -- Edge List, Adjacency List or Adjacency Matrix) using DFS.
+
+# Searching the same graph using BFS.
