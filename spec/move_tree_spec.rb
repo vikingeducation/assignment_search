d3fd5119b8b399aa15354f1eae7da832ca4c3f32
Steven Chang <prime_pork@hotmail.com>
2016-01-25 09:22:03 +1000

    #create_children tests passed re: making grandchildren(recursive) and that depth is increased per generation

--- spec/move_tree_spec.rb
+++ spec/move_tree_spec.rb
@@ -3,6 +3,7 @@ require 'move_tree'
 describe MoveTree do
 
   let(:mid){MoveTree.new([4,4], 1)}
+  let(:mid_two){MoveTree.new([4,4], 2)}
   let(:bottom_left){MoveTree.new([0,0], 1)}
   let(:top_left){MoveTree.new([0,7], 1)}
   let(:top_right){MoveTree.new([7,7], 1)}
@@ -110,6 +111,68 @@ describe MoveTree do
     it "does not receive #add_child_top_left" do
       expect(top_right).to_not receive(:add_child_top_left)
       top_right.create_children(top_right.root_node)
+    end
+
+    it "does not receive #add_child_bottom_left" do
+      expect(bottom_right).to_not receive(:add_child_bottom_left)
+      bottom_right.create_children(bottom_right.root_node)
+    end
+
+    it "does not receive #add_child_bottom_left" do
+      expect(top_left).to_not receive(:add_child_bottom_left)
+      top_left.create_children(top_left.root_node)
+    end
+
+    it "does not receive #add_child_bottom_left" do
+      expect(bottom_right).to_not receive(:add_child_bottom_left)
+      bottom_right.create_children(bottom_right.root_node)
+    end
+
+    it "does not receive #add_child_bottom_left" do
+      expect(top_left).to_not receive(:add_child_bottom_left)
+      top_left.create_children(top_left.root_node)
+    end
+
+    it "does not receive #add_child_left_bottom" do
+      expect(bottom_right).to_not receive(:add_child_left_bottom)
+      bottom_right.create_children(bottom_right.root_node)
+    end
+
+    it "does not receive #add_child_left_bottom" do
+      expect(top_left).to_not receive(:add_child_left_bottom)
+      top_left.create_children(top_left.root_node)
+    end
+
+    it "does not receive #add_child_left_top" do
+      expect(top_right).to_not receive(:add_child_left_top)
+      top_right.create_children(top_right.root_node)
+    end
+
+    it "does not receive #add_child_left_top" do
+      expect(bottom_left).to_not receive(:add_child_left_top)
+      bottom_left.create_children(bottom_left.root_node)
+    end
+=begin
+    # This will need further testing.
+    it "calls #create_children" do
+      expect(mid).to receive(:create_children).at_least(1).times
+      mid.create_children(mid.root_node)
+    end
+=end
+    it "makes grandchildren if depth is 2" do
+      expect(mid_two.root_node.children[0].children[0]).to_not eq(nil)
+    end
+
+    it "stops at grandchildren if depth is 2" do
+      expect(mid_two.root_node.children[0].children[0].children[0]).to eq(nil)
+    end
+
+    it "sets the depth of children to 1" do
+      expect(mid_two.root_node.children[0].depth).to eq(1)
+    end
+
+    it "sets the depth of grandchildren to 2" do
+      expect(mid_two.root_node.children[0].children[0].depth).to eq(2)
     end
   end
 end
