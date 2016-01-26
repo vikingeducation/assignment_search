bb0cee5fe9a3d36cb905f64c51070cd33816b05e
Steven Chang <prime_pork@hotmail.com>
2016-01-26 10:19:56 +1000

    tests passed for knight_searcher # output_results_for_bfs

--- spec/knight_searcher_spec.rb
+++ spec/knight_searcher_spec.rb
@@ -22,6 +22,24 @@ describe KnightSearcher do
       expect{ks_bot_left.bfs_for([3,3])}.to_not raise_error
     end
 
+    it "calls #output_results_for_bfs" do
+      allow(ks_bot_left).to receive(:output_results_for_bfs)
+      expect(ks_bot_left).to receive(:output_results_for_bfs)
+      ks_bot_left.bfs_for([3,4])
+    end
+
+  end
+
+  describe "#output_results_for_bfs" do
+    it "outputs a standard message if answer can't be found" do
+      expect { ks_bot_left.output_results_for_bfs([]) }.to output("Sequence does not exist in that tree, increase the depth and try again!\n").to_stdout
+    end
+
+    it "calls the #sequece_of_moves method if the queue isn't empty" do
+      allow(ks_bot_left).to receive(:sequence_of_moves).and_return([1,1])
+      expect(ks_bot_left).to receive(:sequence_of_moves)
+      ks_bot_left.output_results_for_bfs([(MoveTree.new([0,0], 1)).root_node])
+    end
   end
 
 end
