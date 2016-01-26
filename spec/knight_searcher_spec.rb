20ec6d69efa21e98852760051ee62e6ddd62af3f
Steven Chang <prime_pork@hotmail.com>
2016-01-26 10:31:42 +1000

    tests for #dfs_for in knight_searcher.rb passed

--- spec/knight_searcher_spec.rb
+++ spec/knight_searcher_spec.rb
@@ -31,6 +31,7 @@ describe KnightSearcher do
   end
 
   describe "#output_results_for_bfs" do
+
     it "outputs a standard message if answer can't be found" do
       expect { ks_bot_left.output_results_for_bfs([]) }.to output("Sequence does not exist in that tree, increase the depth and try again!\n").to_stdout
     end
@@ -40,6 +41,37 @@ describe KnightSearcher do
       expect(ks_bot_left).to receive(:sequence_of_moves)
       ks_bot_left.output_results_for_bfs([(MoveTree.new([0,0], 1)).root_node])
     end
+
+  end
+
+  describe "#sequence_of_moves" do
+
+    it "does not raises an error if ONE argument is not given" do
+      expect{ks_bot_left.sequence_of_moves(MoveTree.new([0,0], 1).root_node)}.to_not raise_error
+    end
+
+    it "returns an array" do
+      expect(ks_bot_left.sequence_of_moves(MoveTree.new([0,0], 1).root_node)).to be_a Array
+    end
+
+    it "returns [[0,0]]" do
+      expect(ks_bot_left.sequence_of_moves(MoveTree.new([0,0], 1).root_node)).to eq [[0,0]]
+    end
+
+  end
+
+  describe "#dfs_for" do
+
+    it "does not raise an error if ONE argument is given" do
+      expect{ ks_bot_left.dfs_for([1,1]) }.to_not raise_error
+    end
+
+    it "calls #output_results_for_dfs" do
+      allow(ks_bot_left).to receive(:output_results_for_dfs)
+      expect(ks_bot_left).to receive(:output_results_for_dfs)
+      ks_bot_left.dfs_for([3,4])
+    end
+
   end
 
 end
