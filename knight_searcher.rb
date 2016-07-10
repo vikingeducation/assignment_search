6f7e840df4d5d10fdea5cc8f9397aeeff911ab27
Deepak <grc.deepak@gmail.com>
2016-07-10 11:54:23 -0700

    dfs done

--- knight_searcher.rb
+++ knight_searcher.rb
@@ -17,49 +17,66 @@ class KnightSearcher
   end
 
   def bfs_for(target_coords)
+    node = bfs(target_coords)
+    until node.nil?
+      @result.unshift(node)
+      node = node.parent
+    end
+    if !@result.empty?
+      puts "BFS in #{@result.size-1} Moves:"
+      @result.each { |node| puts "[#{node.x}, #{node.y}]"}
+    else
+      puts "BFS did not find the target_coords #{target_coords}"
+    end
+  end
+
+  def bfs(target_coords)
     root = @tree.root
     @queue << root
     until @queue.empty?
       node = @queue.shift
-      break if node.x == target_coords.first && node.y == target_coords.last
+      return node if node.x == target_coords.first && node.y == target_coords.last
       node.children.each do |child|
         next if child.nil?
         @queue << child
       end
     end
+    nil
+  end
 
+  def dfs_for(target_coords)
+    node = dfs(target_coords)
     until node.nil?
       @result.unshift(node)
       node = node.parent
     end
-    puts "#{@result.size-1} Moves:"
-    @result.each { |node| puts "[#{node.x}, #{node.y}]"}
+    if !@result.empty?
+      puts "DFS in #{@result.size-1} Moves:"
+      @result.each { |node| puts "[#{node.x}, #{node.y}]"}
+    else
+      puts "DFS did not find the target_coords #{target_coords}"
+    end
   end
-  # def bfs_for(target_coords)
-  #   root = @tree.root
-  #   height = @tree.height
-  #   (0..height).each do |i|
-  #     search_given_level(root, i, target_coords)
-  #   end
-  # end
+end
 
-  # def search_given_level(root, level, target_coords)
-  #   return if root.nil?
-  #   if level == 0
-  #     puts "#{root.x}, #{root.y}"
-  #   elsif level > 0
-  #     root.children.each do |child|
-  #       search_given_level(child, level-1, target_coords);
-  #     end
-  #   end
-  # end
-
-  def dfs_for(target_coords)
-
+def dfs(target_coords)
+  root = @tree.root
+  @stack << root
+  until @stack.empty?
+    node = @stack.pop
+    return node if node.x == target_coords.first && node.y == target_coords.last
+    node.children.each do |child|
+      next if child.nil?
+      @stack << child
+    end
   end
-
+  nil
 end
 
 knight_tree = MoveTree.new([3, 3], 4)
 searcher = KnightSearcher.new(knight_tree)
 searcher.bfs_for([1,3])
+searcher.dfs_for([1,3])
+KnightSearcher.new(MoveTree.new([0,0], 6)).dfs_for([6,0])
+KnightSearcher.new(MoveTree.new([0,0], 6)).bfs_for([6,0])
+
