3129fefded04acd9eb515e845ff9efad44a57843
“Matthew <matthew.hinea@gmail.com>
2016-07-14 18:53:06 -0400

    developing moves tree

--- knight.rb
+++ knight.rb
@@ -4,21 +4,43 @@ VECTORS = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2
 
 class MoveTree
 
-  def initialize(starting, depth)
-    @starting_pos = starting
+  def initialize(starting)
+    @starting_pos = Move.new(starting[0], starting[1])
     @depth = depth
   end
 
-  def build_move(x, y, depth)
-    Move.new(x, y, depth)
+  def build_move(x, y)
+    Move.new(x, y)
   end
 
-  def moves_array
-    # From any position we can move + or - 2 to either x or y and + or - 1 to the other direction.
-    # potential x's are x -2 and x + 2
-    # potential y's are y - 1 and y + 1
+  def build_trees(starting_pos)
+    @depth.times do 
+      moves = moves_array(starting_pos)
+      moves.map! do |move| 
+        Move.new(move[0], move[1])
+      end
+      moves.each do |move|
+        add_move(move)
+      end
+    end
+  end
 
+  def add_move(move)
+    current_lvl = @starting_pos
+    loop do
+      
+    end
+  end
 
+  def moves_array(starting_pos)
+    x = starting_pos[0]
+    y = starting_pos[1]
+    possible_moves = []
+    VECTORS.each do |move|
+      possible_moves << [x + move[0], y + move[1]]
+    end
+    # iterate through possible_moves, removing any x,y pair in which either x or y is greater than 7 or less than 0
+    possible_moves.select {|move| move[0] < 7 && move[1] < 7 && move[0] >= 0 && move[1] >= 0 }
   end
 
 end
