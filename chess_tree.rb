2fca12f9377312075085bc269fdf100ed2ab4a49
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-14 16:27:17 -0700

    Chugging along

--- chess_tree.rb
+++ chess_tree.rb
@@ -2,10 +2,11 @@ Node = Struct.new(:x, :y, :depth, :children, :parent)
 
 class ChessTree
   attr_reader :size, :max_depth, :start, :root
-  POSSBILE_MOVES = [[1,2],[2,1], [-1, -2], [-2, -1],
+  POSSIBLE_MOVES = [[1,2],[2,1], [-1, -2], [-2, -1],
                     [-1, 2], [2, -1], [1,-2],[-2, 1]]
 
   def initialize(size, max_depth)
+    @total_children = 0
     @root = nil
     @size = size
     @max_depth = max_depth
@@ -28,12 +29,28 @@ class ChessTree
     @root = Node.new(start[0], start[1], 0, [], nil)
   end
 
+  def create_tree
+    make_root
+    current_node = @root
+    counter = 1
+    until counter > max_depth
+      make_children(current_node)
+      current_node.children.each do |child|
+        make_children(child)
+      end
+
+      counter += 1
+    end
+  end
+
   def make_children(node)
+    return nil if node.depth > @max_depth
     POSSIBLE_MOVES.each do |move|
       if (node.x + move[0] < size && node.y + move[1] < size) &&
           (node.x + move[0] >= 0 && node.y + move[1] >= 0)
         child = Node.new(node.x + move[0], node.y + move[1], node.depth + 1, [], node)
-        node.children << child if !check_target?(child)
+        node.children << child
+        @total_children += 1
       else
         next
       end
