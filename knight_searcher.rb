9c650a5d116e8e291d15b33caf509a2cbac6fa0e
cjvirtucio87 <cjv287@gmail.com>
2016-07-14 16:47:34 -0700

    Attempted recursion.

--- knight_searcher.rb
+++ knight_searcher.rb
@@ -2,13 +2,14 @@ require 'pry'
 require_relative 'knight.rb'
 
 class KnightSearcher
+  attr_reader :tree
 
   def initialize(tree)
     @tree = tree
   end
 
   def bfs_for(array)
-    found_move = find_move(array)
+    found_move = bfs_find_move(array)
     move = found_move
     if move
       puts "Found the coordinate at depth level #{move.depth}."
@@ -21,7 +22,43 @@ class KnightSearcher
     end
   end
 
-  def find_move(array)
+  def dfs_for(array)
+    found_move = dfs_find_move(array,@tree.starting_move)
+    if found_move
+      puts "Found the coordinate at depth level #{found_move.depth}."
+    else
+      raise "Couldn't find coordinate."
+    end
+    while found_move.parent
+      puts "[#{found_move.parent.x}, #{found_move.parent.y}]"
+      found_move = found_move.parent
+    end
+  end
+
+  
+
+  # def dfs_find_move(array,move)
+  #   if move.children.nil?
+  #     return
+  #   elsif array == [move.x,move.y]
+  #     return move
+  #   else
+  #     move.children.each do |child|
+  #       dfs_find_move(array,child)
+  #     end
+  #   end
+  # end
+
+# Recursive
+# search(value, node)
+#   return node if value = node.value
+#   until node.children is nil
+#   node.children.each do |child|
+#     search(value, child)
+#   end
+# end
+
+  def bfs_find_move(array)
     current = @tree.starting_move
     queue = [current]
     until queue.empty?
@@ -39,8 +76,8 @@ end
 
 
 tree = MoveTree.new([2,2], 3)
-# tree = empty_tree.move_tree
-
 k = KnightSearcher.new(tree)
 
-k.bfs_for([3,7])
+
+k.dfs_for([4,3])
+# puts k.dfs_find_move([4,3],k.tree.starting_move)
