d571f1b25159e4b945ef8811986bd30919f548bf
Hannah Squier <hannahsquier@gmail.com>
2016-07-14 16:42:38 -0700

    create get_children method and split up add_moves

--- knights_travail.rb
+++ knights_travail.rb
@@ -2,10 +2,10 @@ Move = Struct.new(:x, :y, :depth, :children, :parent)
 #Cell = Struct.new(:x, :y, :empty)
 
 class MoveTree
-
+  attr_reader :root
   def initialize(coords, max_depth)
     @root = Move.new(coords[0], coords[1], 0, [], [])
-    @move_count = 0
+    @move_count = 1
     @max_depth = max_depth
     @move_tree = add_moves #array of move structs
     
@@ -21,35 +21,15 @@ class MoveTree
     #( x-2, y+1 )
     #( x+1, y-2 )
     #( x-1, y+2 )
-    current_move = @root
-    current_move2 = @root
-    p current_move.children
-    until current_move.depth >= @max_depth && current_move2.depth >= @max_depth
-      [-2, 2].each do |delta2|
-        [-1, 1].each do |delta1|
-          
-          x_coord = current_move.x + delta2 
-          y_coord = current_move.y + delta1
 
-          x_coord2 = current_move.x + delta1
-          y_coord2 = current_move.y + delta2
-          
-          if x_coord.between?(0, 7) && y_coord.between?(0, 7)
-            next_move = Move.new(x_coord, y_coord, current_move.depth + 1, [], current_move)
-            p current_move.children
-            current_move.children << next_move
-            current_move = next_move
-            @move_count += 1
-          end
 
-          if x_coord2.between?(0, 7) && y_coord2.between?(0, 7)
-            next_move2 = Move.new(x_coord2, y_coord2, current_move2.depth + 1, [], current_move2)
-            current_move2.children << next_move2
-            current_move2 = next_move2
-            @move_count += 1
-          end
-          
-        end
+    parent = @root
+
+    until parent.depth >= @max_depth && parent.depth >= @max_depth
+      get_children(parent)
+      parent.children.each do |child|
+        parent = child
+        get_children(parent)
       end
     end
     @root
@@ -58,20 +38,58 @@ class MoveTree
   def inspect
     puts "Your tree has #{@move_count} Move nodes and a maximum depth of #{@max_depth}."
   end
+
+  def get_children(parent)
+    x_coord = parent.x
+    y_coord = parent.y
+    x_coord2 = parent.x
+    y_coord2 = parent.y
+
+    [-2, 2].each do |delta2|
+      [-1, 1].each do |delta1|
+        
+           puts "parent x:#{parent.x}, parent y: #{parent.y}" 
+        if x_coord.between?(0, 7) && y_coord.between?(0, 7)
+          x_coord = parent.x + delta2 
+          y_coord = parent.y + delta1
+          
+
+          
+          puts "x:#{x_coord}, y: #{y_coord}"
+          next_move = Move.new(x_coord, y_coord, parent.depth + 1, [], parent)
+          parent.children << next_move
+          @move_count += 1
+        end
+
+        if x_coord2.between?(0, 7) && y_coord2.between?(0, 7)
+          x_coord2 = parent.x + delta1
+          y_coord2 = parent.y + delta2  
+
+          puts "x:#{x_coord2}, y: #{y_coord2}"
+          next_move2 = Move.new(x_coord2, y_coord2, parent.depth + 1, [], parent)
+          parent.children << next_move2
+          @move_count += 1
+        end
+        
+      end
+    end
+    parent
+  end
 end
 
 
 m = MoveTree.new([4, 4], 1)
-p m.add_moves
+parent = m.root
+p m.get_children(parent)
 m.inspect
 
 #x = 4, y = 4
 
 # 3, 6
-# 3, 2
+# 3, 2 yes
 # 5, 6
 # 5, 2
 # 2, 5
-# 2, 3
+# 2, 3 yes
 # 6, 5
 # 6, 3
