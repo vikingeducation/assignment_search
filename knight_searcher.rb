00d763946fda721d6bc308388c365f0ffe88a39c
Leo Ahnn <leoahnn@gmail.com>
2016-07-14 19:10:45 -0400

    added build tree

---
+++ knight_searcher.rb
@@ -0,0 +1,17 @@
+#knight_searcher.rb
+
+class KnightSearcher
+
+  def initialize( knight_tree )
+    @tree = knight_tree
+  end
+
+# input target coordinates and outputs sequence of moves and final depth
+  def bfs_for(coords)
+  end
+
+
+  def dfs_for(coords)
+  end
+
+end
--- move_tree.rb
+++ move_tree.rb
@@ -1,34 +1,58 @@
 # knights_travails.rb
-
+require_relative 'knight_searcher'
+require "pry"
 Move = Struct.new(:x, :y, :depth, :children, :parent)
 
 #tree of moves
 
 #construct a tree of potential moves from a given position using Move
-#two inputs: 
+#two inputs:
 #coordinate pair x*, y* to represent starting position
 #max_depth  depth* to prevent tree from infinite loops
-
-
-
+# 8x8 chess board
 class MoveTree
   attr_reader :coord, :max_depth, :root
 
   def initialize(coord, max_depth)
-    @root = Move.new(coord[0], coord[1], 0, nil, nil)
-
+    @root = Move.new(coord[0], coord[1], 0, [], nil)
     @coord = coord #probably won't need this
     @max_depth = max_depth
-    
   end
-
-  #total # of nodes and max depth 
 
   def inspect
     p "Your tree has #{something} Move nodes and a maxium depth of #{@max_depth}"
   end
 
+  def build_tree
+    holder = [@root]
+    depth = @root.depth
+    until depth >= max_depth
+      depth = holder[0].depth + 1 #depth of child
+      holder.each do |holder_move|
+        # iterates to add all possible moves
+        possible_moves([holder_move.x, holder_move.y]).each do |arr|
+          arr.each_slice(2) do |x,y|
+            # binding.pry
+            holder_move.children << Move.new(x, y, depth, [], holder) if valid_move?(x, y)
+          end
+        end
+      holder = holder[0].children
+      end
+    end
+  end
+
+  def valid_move?(x, y)
+    x.between?(0, 7) && y.between?(0, 7)
+  end
+
+  def possible_moves(parent_coord)
+    x = parent_coord[0]
+    y = parent_coord[1]
+    [[x+2, y+1], [x+2, y-1], [x-2, y+1], [x-2, y-1],
+    [x+1, y+2], [x+1, y-2], [x-1, y+2], [x-1, y-2]]
+  end
 end
 
-
-
+m = MoveTree.new([0,0],2)
+m.build_tree
+m.root
