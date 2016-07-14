7a3a5f7a9deb9474137b67402727a8f5a65edad5
jsteenb2 <jonathansteenbergen@gmail.com>
2016-07-14 16:18:03 -0700

    added crawler algorithm

--- knight.rb
+++ knight.rb
@@ -13,18 +13,41 @@ class MoveTree
   attr_reader :coordinates, :depth, :root
   def initialize(coordinates, depth)
     starting_point = Move.new(coordinates[0], coordinates[1], 0, [], nil)
+    @max_depth = depth
     @root = starting_point
     @directions = [[2,1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]
     populate(starting_point)
     @root.children.each {|child| print "#{child.x}, #{child.y}\n" }
   end
 
+  def build_tree
+    =begin
+    current_node = @root
+    stack_arr = []
+    stack_arr.push(current_node)
+    until stack_arr.empty?
+      populate(current_node)
+      until stack_arr
+      current_node.children.each do |child|
+          stack_arr.push(child)
+      end
+      stack_arr.shift
+      current_node = stack_arr[0]
+
+    =end
+
+    until current_node.depth == @max_depth
+      return
+
+
+  end
+
   def populate(current_node)
     possible_moves = find_next_move(current_node)
+    return false if possible_moves.empty?
     possible_moves.each do |move|
       current_node.children << move
     end
-
   end
 
   def find_next_move(current_node)
@@ -40,7 +63,7 @@ class MoveTree
   def valid_move?(current_node, direction)
     current_node.x + direction[0] >= 0 && current_node.x + direction[0] < 5 && current_node.y + direction[1] >= 0 && current_node.y + direction[1] < 5
   end
-  
+
 
 
 end
