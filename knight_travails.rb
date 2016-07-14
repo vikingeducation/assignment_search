02a5dc22b44f1ee754cffa7e5cc418ab3c1eeed8
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-14 17:32:31 -0500

    adde to readme

---
+++ knight_travails.rb
@@ -0,0 +1,40 @@
+# Searching a simple tree of nodes where each Node has an array of child nodes (some_node.children) using DFS.
+
+# a method that that takes a node 
+# node has array
+# itererate through the array
+# at Node[0] check if answer
+# if not answer
+# call recursivly the method again on current node
+# if Node[0] is nil
+#   check if previous has child more than one child 
+#   recursivly call method on next node else continue up
+
+
+
+
+# Searching the same tree using BFS.
+
+  find root node add to queue
+  start pulling from queue
+    loop pull node off queue and return if it matches
+         add current nodes children to queue
+        end loop
+        return no find
+
+
+
+
+
+
+# Searching a graph (represented however you feel most comfortable -- Edge List, Adjacency List or Adjacency Matrix) using DFS.
+
+
+    
+
+
+
+
+
+# Searching the same graph using BFS.
+
