fe03ebd561f6e5c69a2ab29d9465faf80fb25a51
essian <jessicampdennis@hotmail.com>
2016-07-15 01:02:08 +0100

    starts knights travail

--- moves.rb
+++ moves.rb
@@ -1,3 +1,16 @@
+# initialize a movetree with co-ords and max depth
+# build tree
+  # create a queue
+  # add starting node to be co-ords and add depth to be 0
+  # pop from queue set as current node
+  # find valid moves for current node
+    # set as children, and add to queue with depth(parent's depth + 1), parent
+    # break if max depth reached
+
+  
+
+
+
 Move = Struct.new(:x, :y, :depth, :children, :parent)
 
 class MoveTree
@@ -30,8 +43,11 @@ class MoveTree
    invalid_coordinate?(x) || invalid_coordinate?(y) 
   end
 
-  def next_coordinate(x,y)
-    get_coordinates.reject {|coordinates| invalid_move?}
+  def next_coordinates(x,y)
+    get_coordinates(x, y).reject {|coordinates| invalid_move?(*coordinates)}
   end
 
 end
+
+t = MoveTree.new(1,2)
+p t.next_coordinates(0,0)
