cb82266c8e534023cb2ba91ad00dce8881e18ca3
Alex Lach <alexglach@gmail.com>
2016-07-14 21:00:09 -0400

    re-did a lot with kit's help

--- knight.rb
+++ knight.rb
@@ -7,7 +7,18 @@
 # =>  if no moves available, go to starting position's next child node (DFS)
 require 'pry'
 
-Move = Struct.new(:x, :y, :depth, :children, :parents)
+Move = Struct.new(:x, :y, :depth, :children, :parent)
+class Move
+
+  def to_s
+    "#{x}, #{y}:\n #{children}"
+  end
+
+  def inspect
+    "#{x}, #{y}:\n #{children}"
+  end
+
+end
 
 class MoveTree
 
@@ -17,23 +28,24 @@ class MoveTree
     @max_depth = depth
     @root = starting_point
     @directions = [[2,1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]
+    @visited_coordinates = {}
     build_tree
-    @root.children[0].children[0].children[0].children.each do |child|
-      puts "#{child.x}, #{child.y}\n"
-    end
+    # @root.children[0].children[0].children[0].children.each do |child|
+    #   puts "#{child.x}, #{child.y}\n"
+    puts @root
   end
 
+
   def build_tree
-    current_node = @root
-    stack_arr = []
-    stack_arr.push(current_node)
-    until stack_arr.empty?
+  current_node = @root
+  stack_arr = []
+  stack_arr.push(current_node)
+    # until stack_arr.empty?
+    while current_node = stack_arr.shift
       populate(current_node)
       current_node.children.each do |child|
-          stack_arr.push(child)
+        stack_arr.push(child)
       end
-      stack_arr.shift
-      current_node = stack_arr[0]
     end
 
   end
@@ -43,31 +55,33 @@ class MoveTree
     return false if possible_moves.empty?
     # binding.pry
     possible_moves.each do |move|
-      move.parents << current_node
       current_node.children << move
     end
   end
 
   def find_next_move(current_node)
     possible_moves = []
-    @directions.each do |direction|
-      if valid_move?(current_node, direction) && not_visited?(current_node, direction)
-        possible_moves << Move.new(current_node.x + direction[0], current_node.y + direction[1], current_node.depth + 1, [], current_node.parents)
+    next_coordinates(current_node).each do |next_x, next_y|
+      if valid_move?(next_x, next_y) && not_visited?(next_x, next_y)
+        possible_moves << Move.new(next_x, next_y, current_node.depth + 1, [], current_node)
+        @visited_coordinates[[next_x, next_y]] = "Hooray!"
       end
     end
     possible_moves
   end
 
-  def not_visited?(current_node, direction)
-    return true if current_node == @root
-    current_node.parents.none? do |parent|
-      (parent.x == current_node.x + direction[0]) && (parent.y == current_node.y + direction[1])
-    end
+  def next_coordinates(current_node)
+    @directions.map {|x,y| [current_node.x + x, current_node.y + y]}
+  end
+
+  def not_visited?(next_x, next_y)
+    !@visited_coordinates.include?([next_x, next_y])
+    
   end
 
 
-  def valid_move?(current_node, direction)
-    (current_node.x + direction[0] >= 0) && (current_node.x + direction[0] < 5) && (current_node.y + direction[1] >= 0) && (current_node.y + direction[1] < 5)
+  def valid_move?(next_x, next_y)
+    next_x >= 0 && next_x < 5 && next_y >= 0 && next_y < 5
   end
 
 
