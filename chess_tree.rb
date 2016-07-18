8bf79b32cc59b8b22fd0466f555b22e49c4d5a24
Dylan Lynch <lynchd2@tcnj.edu>
2016-07-18 00:34:29 -0400

    Breadth first search complete for knight travails. Add render moves

--- chess_tree.rb
+++ chess_tree.rb
@@ -8,7 +8,7 @@ class ChessTree
   def initialize(size, max_depth)
     @total_children = 0
     @root = nil
-    @size = size
+    @size = size + 1
     @max_depth = max_depth
     @start = ""
     @finish = ""
@@ -64,38 +64,5 @@ class ChessTree
 
   def check_target?(child)
     child.x == @finish[0] && child.y = @finish[1]
-  end
-
-
-  def insert(x, y)
-    if @root.nil?
-      add_root(x, y)
-    else
-      node = nodify(x, y)
-      add_node(node)
-    end
-  end
-  
-  def nodify(x, y)
-    node.new(x, y, nil, [])
-  end
-
-  def add_root(x, y)
-    @root = nodify(x, y, 0, [])
-  end
-
-  def add_node(node)
-    current_node = @root
-    parent = nil
-    until current_node.nil?
-      parent = current_node
-      if node.value < current_node.value
-        current_node = current_node.left
-      else
-        current_node = current_node.right
-      end
-    end
-
-    node.value < parent.value ? parent.left = node : parent.right = node
   end
 end
--- knight_searcher.rb
+++ knight_searcher.rb
@@ -1,24 +1,35 @@
+require_relative "chess_tree"
+
 class KnightSearcher
-  def initialize(tree)
-    @tree = tree
+  def initialize
   end
 
-
-  def bfs_for(target)
-    queue = []
-    queue << @tree.root
-    unless queue[0].x == target.x && queue[0].y == target.y
-      if queue[0].children == []
+  def bfs_for(root, target)
+    queue = [root]
+    until queue[0].x == target[0] && queue[0].y == target[1]
+      unless queue[0].children == nil
+        queue[0].children.each do |child|
+          queue.push(child)
+        end
         queue.shift
       end
-      queue[0].children.each do |child|
-        queue.push(child)
-      end
-    queue.shift
+    end
+    current_node = queue[0]
+    moves = []
+    until current_node.parent == nil
+      moves << [current_node.x, current_node.y]
+      current_node = current_node.parent
+    end
+    count = 1
+    moves.reverse.each do |move|
+      puts "Move #{count}- X: #{move[0]} Y: #{move[1]}" 
+      count += 1
+    end
   end
-
 end
-
-
-
-searcher = KnightSearcher.new(ChessTree.new(5,10))
+tree = ChessTree.new(30,8)
+root = tree.make_root
+target = tree.ask_target
+tree.make_children(tree.root)
+searcher = KnightSearcher.new()
+searcher.bfs_for(root, target)
