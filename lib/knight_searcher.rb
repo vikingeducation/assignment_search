5ad38f95838230398256bd7fa0cff5fe33bf26f3
“Matthew <matthew.hinea@gmail.com>
2016-07-18 10:44:38 -0400

    refactored match? parameter names for clarity

--- lib/knight_searcher.rb
+++ lib/knight_searcher.rb
@@ -8,14 +8,6 @@ class KnightSearcher
   end
 
   def bfs_for(target_coords)
-    # Upon locating the target square, the method should output 
-    # the sequence of moves used to get there as well as the final depth required.
-
-    # if start.x,y are target, return start
-    # else, iterate through children of start
-    # if a child.x,y are target, return that child
-    # if no children.x,y are target, iterate through all the children of all the children of start
-    # etc
     queue = [@start]
     while node = queue.pop
       if match?(node, target_coords)
@@ -38,8 +30,8 @@ class KnightSearcher
     end
   end
 
-  def match?(current, target) 
-    return true if current.x == target[0] && current.y == target[1]
+  def match?(current, target_coords) 
+    return true if current.x == target_coords[0] && current.y == target_coords[1]
     false
   end
 
