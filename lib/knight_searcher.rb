da70934b34018d1dc07cc6e9b9ead725c408f8b8
“Matthew <matthew.hinea@gmail.com>
2016-07-18 10:14:06 -0400

    fixed typo in knightsearcher causing infinite search loop

--- lib/knight_searcher.rb
+++ lib/knight_searcher.rb
@@ -7,7 +7,7 @@ class KnightSearcher
     @max_depth = moves_tree.max_depth
   end
 
-  def bfs_for(start, target_coords)
+  def bfs_for(target_coords)
     # Upon locating the target square, the method should output 
     # the sequence of moves used to get there as well as the final depth required.
 
@@ -16,18 +16,16 @@ class KnightSearcher
     # if a child.x,y are target, return that child
     # if no children.x,y are target, iterate through all the children of all the children of start
     # etc
-    queue = [start]
-    binding.pry
+    queue = [@start]
     while node = queue.pop
 
       if match?(node, target_coords)
         puts "Found after #{node.depth} moves."
         puts "Move sequence (backwards from target):"
         puts "[#{node.x}, #{node.y}]"
-
         return node 
       else
-        start.children.each do |child|
+        node.children.each do |child|
           queue.unshift(child)
         end
       end 
