4e533b13ffd7ba8ff5d311040597d8800bbe8723
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-17 17:26:46 -0700

    Implements BF and DF searches

--- chess_tree.rb
+++ chess_tree.rb
@@ -1,18 +1,16 @@
 Node = Struct.new(:x, :y, :depth, :children, :parent)
 
 class ChessTree
-  attr_reader :size, :max_depth, :start, :root, :initial_depth 
+  attr_reader :size, :max_depth, :start, :root 
   POSSIBLE_MOVES = [[1,2],[2,1], [-1, -2], [-2, -1],
                     [-1, 2], [2, -1], [1,-2],[-2, 1]]
 
-  def initialize(size, max_depth)
+  def initialize(size, max_depth, start = [0, 0])
     @total_children = 0
     @root = nil
     @size = size
     @max_depth = max_depth
-    @start = ""
-    @finish = ""
-    @initial_depth = 0
+    @start = start
   end
 
   def ask_start
@@ -20,41 +18,23 @@ class ChessTree
     @start = gets.chomp.split(",").map(&:to_i)
   end
 
-  def ask_target
-    puts "Where do you want to finish?(pick an x, y coordinate)"
-    @finish = gets.chomp.split(",").map(&:to_i)
-  end
-
   def make_root
-    start = ask_start
-    @root = Node.new(start[0], start[1], 0, [], nil)
+    @root = Node.new(@start[0], @start[1], 0, [], nil)
   end
 
-=begin
   def create_tree
     make_root
-    current_node = @root
-    make_children(current_node)
-    counter = 1
-    until counter > max_depth
-      current_node.children.each do |child|
-        make_children(child)
-        counter += 1
-        current_node = child
-      end
-    end
+    make_children(@root)
+    self
   end
-=end
 
   def make_children(node)
-    #return nil if node.depth > @max_depth
     POSSIBLE_MOVES.each do |move|
       if (node.x + move[0] < size && node.y + move[1] < size) &&
           (node.x + move[0] >= 0 && node.y + move[1] >= 0)
         child = Node.new(node.x + move[0], node.y + move[1], node.depth + 1, [], node)
         node.children << child
         @total_children += 1
-        @initial_depth += 1
         make_children(child) unless child.depth == @max_depth
       else
         next
--- knight_searcher.rb
+++ knight_searcher.rb
@@ -1,3 +1,5 @@
+require_relative 'chess_tree'
+
 class KnightSearcher
   def initialize(tree)
     @tree = tree
@@ -6,19 +8,73 @@ class KnightSearcher
 
   def bfs_for(target)
     queue = []
-    queue << @tree.root
-    unless queue[0].x == target.x && queue[0].y == target.y
-      if queue[0].children == []
+    queue.push(@tree.root)
+    until queue.empty?
+      if queue[0].x == target[0] && queue[0].y == target[1]
+        return list_moves(queue[0])
+      else
+        queue[0].children.each do |child|
+          queue.push(child)
+        end
         queue.shift
       end
-      queue[0].children.each do |child|
-        queue.push(child)
+    end
+  end
+
+  def dfs_for(target)
+    stack = []
+    stack.push(@tree.root)
+    until stack.empty?
+      current = stack.pop
+      if current.x == target[0] && current.y == target[1]
+        return list_moves(current)
+      else
+        current.children.each do |child|
+          stack.push(child)
+        end
       end
-    queue.shift
+    end
+  end
+
+  def list_moves(move)
+    moves = []
+    until move.parent.nil?
+      moves << move
+      move = move.parent
+    end
+    moves
+  end
+
+  def print_moves(moves)
+    puts "The knight took #{moves.length} moves."
+    print "Moves: [#{@tree.root.x}, #{@tree.root.y}]"
+    until moves.empty? do
+      current = moves.pop
+      print " -> [#{current.x}, #{current.y}]"
+    end
+    puts
   end
 
 end
 
+def benchmark
+  t = Time.new
+  yield
+  puts "It took #{ Time.new - t} seconds."
+end
 
-
-searcher = KnightSearcher.new(ChessTree.new(5,10))
+c = ChessTree.new(8, 8, [0,0])
+c.create_tree
+searcher = KnightSearcher.new(c)
+puts "Where to?"
+target = gets.chomp.split(",").map(&:to_i)
+benchmark do
+  puts "Breadth:"
+  searcher.print_moves(searcher.bfs_for(target))
+end
+puts
+benchmark do
+  puts "Depth:"
+  searcher.print_moves(searcher.dfs_for(target))
+end
+puts
