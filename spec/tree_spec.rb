a45386c00bb53ceaa9735e526f6ce2b7c2dfad4f
BranLiang <lby89757@hotmail.com>
2016-08-04 23:37:22 +0800

    set up the guard

---
+++ spec/tree_spec.rb
@@ -0,0 +1,11 @@
+require 'tree'
+require 'spec_helper.rb'
+describe MoveTree do
+  let(:mytree){ MoveTree.new([2,3], 1) }
+
+  context '#in_bound?' do
+    it 'filter the unproper nodes' do
+      
+    end
+  end
+end
