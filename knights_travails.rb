4f56641f584325b37831416fb73c643f8a71fb62
Joe Bernardi <joebernardi@hotmail.com>
2016-08-12 13:26:40 -0500

    change children to array

--- knights_travails.rb
+++ knights_travails.rb
@@ -22,28 +22,17 @@ class KnightsTravail
 	end
 
 
-# the root has potentially 8 paths if not on an edge
-	# each move would have to be checked for being within the Board's boundaries
-# with each move, the square should be marked to prevent a cycle
 
-# Board as 8x8 2D Array
-# move is [ [0,0] [1,3] ]
-# Knight will be a class
-	# houses moves he can make
-# Board as a class
-# Binary Tree
-# BFS - for shortest solution
+	def find_moves
 
-# starting with the root node
-# we take that node and place it into a queue
-# we then check the possible moves the knight can make from that position
-# if the move is within boundaries
-	# we add that node to the queue
-# if out of bounds we move to the next possible move
-	# as long as the move is not equal to the last square
-# once a move is Dequeued, we mark if with a MOVED item to indicate already checked and issue a return statement
+		# first we take the first move and add it to the queue
+
+	end
+
+
 
 end
 
 
 game = KnightsTravail.new( [3,4], [6,7] )
+game.find_moves
--- tree.rb
+++ tree.rb
@@ -1,16 +1,96 @@
+require 'pry'
 
-Move = Struct.new( :coord, :moves, :parent )
+
+Move = Struct.new( :x, :y, :depth, :children, :parent )
 
 class Tree
 
-	attr_reader :start
+	attr_reader :coords, :max_depth
 
-	def initialize( start )
+	def initialize( coords, max_depth )
 
-		@start = Move.new( start, [], nil )
+		@start = Move.new( coords[0], coords[1], 0, [], nil )
+		@max_depth = 1
+
+		@depth = 0
 
 	end
 
 
+	def create_moves
+
+		# establish our current node
+		current_position = @start
+		# we then determine the possible moves from this position
+		moves = possible_moves( current_position )
+		@depth += 1
+
+
+		# for each possible move
+		moves.each do | coord |
+
+
+			if valid_move?( coord )
+			# we create a node who's parent is the current node
+				new_node = Move.new( coord[ 0 ], coord[ 1 ], @depth, [], current_position )
+
+				binding.pry
+				current_position.children << new_node
+				current_position = new_node
+
+			else
+
+				return
+
+			end
+
+		end
+
+	end
+
+
+	def possible_moves( current_position )
+
+
+		x = current_position.x
+		y = current_position.y
+
+		arr = [
+						[ x + 2, y - 1 ], [ x + 2, y + 1 ],
+						[ x + 1, y - 2 ], [ x - 1, y - 2 ],
+						[ x - 2, y + 1 ], [ x - 2, y - 1 ],
+						[ x - 1, y + 2 ], [ x + 1, y + 2 ]
+					]
+
+		return arr
+
+	end
+
+
+	def valid_move?( coord )
+
+		return true if ( 0..7 ) === coord[ 0 ] &&
+									 ( 0..7 ) === coord[ 1 ]
+
+	end
+
+
+	def print_tree
+
+		#current_node = @start
+
+	end
+
 
 end
+
+tree = Tree.new( [3,3], 1 )
+
+
+tree.create_moves
+
+
+print tree.inspect
+
+
+#tree.print_tree
