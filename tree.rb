5952299f9b504fc3653bd7c1b2c74759f88b820f
Joe Bernardi <joebernardi@hotmail.com>
2016-08-15 21:08:55 -0500

    try recursive tree

--- tree.rb
+++ tree.rb
@@ -10,22 +10,26 @@ class Tree
 	def initialize( coords, max_depth )
 
 		@start = Move.new( coords[0], coords[1], 0, [], nil )
-		@max_depth = 1
+		@max_depth = 3
 
 		@depth = 0
+
+		create_moves( @start )
 
 	end
 
 
-	def create_moves
+	def create_moves( node )
+
+
+		while @depth < @max_depth
+
 
 		# start with the root node set as current position
-		current_position = @start
-
 		# find all the possible moves for the current position
 		@depth += 1
 
-		moves = possible_moves( current_position )
+		moves = possible_moves( node )
 		#while the depth is less than what's set
 
 			# go through each possible move
@@ -35,10 +39,10 @@ class Tree
 				if valid_move?( coord )
 
 					# if it is, we make a new Node setting the coordinates as the parent of the current node
-					new_node = Move.new( coord[ 0 ], coord[ 1 ], @depth, [], current_position )
+					new_node = Move.new( coord[ 0 ], coord[ 1 ], @depth, [], node )
 
 					# we then add the new_node as a child of the current node
-					current_position.children << new_node
+					node.children << new_node
 
 
 
@@ -49,6 +53,14 @@ class Tree
 				end
 
 			end #/.moves.each
+
+			node.children.each do  | n |
+
+				create_moves( n )
+
+			end
+
+		end #/.while loop
 
 	end
 
@@ -81,7 +93,11 @@ class Tree
 
 	def print_tree
 
-		print " There are #{@start.children.count + 1} nodes and a max depth of #{@max_depth}"
+		@start.children.each do |x|
+
+			puts x.parent
+
+		end
 
 	end
 
@@ -90,7 +106,5 @@ end
 
 tree = Tree.new( [3,3], 1 )
 
-
-tree.create_moves
 
 tree.print_tree
