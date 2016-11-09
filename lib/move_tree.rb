460b1555dea9d640cbd52100b8a572dff29442b3
Francisco Suero <suerofrancisco@yahoo.com>
2016-11-09 18:12:27 -0500

    trees! trees! a veritable forest!

--- lib/move_tree.rb
+++ lib/move_tree.rb
@@ -4,8 +4,10 @@
 require_relative 'move'
 
 class MoveTree
+  attr_reader :root 
 
   def initialize(coordinates, max_depth)
+    #assumes input is valid, please please please. Thanks.
     @coordinates = coordinates
     @max_depth   = max_depth
     @upper_limit = 7
@@ -17,14 +19,24 @@ class MoveTree
   def moves_from(coordinates = @coordinates, max_depth = @max_depth)
     # construct tree of nodes (moves) from a given position
     x, y = coordinates
-    current_node = Move.new(x, y, nil, nil)
     current_depth = 0
+    current_node = @root = Move.new(x, y, current_depth, [])
     while current_depth < max_depth
-      move_posibilities.each do |position_delta|
-        unless out_of_bounds(current_node, position_delta)
+      x, y = current_node.x, current_node.y
+      move_posibilities.each do |delta|
+        unless out_of_bounds([x, y], delta)
+          new_node = make_move_node(x, y, delta, current_depth)
+          current_node.child = new_node
+          new_node.parent = current_node
         end
       end
+      current_depth += 1
     end
+  end
+
+  def make_move_node(x, y, delta, depth)
+    delta_x, delta_y = delta
+    Move.new(x + delta_x, y + delta_y, depth, [])
   end
 
   def move_posibilites
@@ -33,8 +45,12 @@ class MoveTree
   end
 
   def out_of_bounds(start, delta)
-    delta_x, delta_y = position_delta
-    x + delta_x > @right_limit ||
+    x, y = start
+    delta_x, delta_y = delta
+    x + delta_x > @right_limit ||  x + delta_x < @left_limit ||
+    y + delta_y > @upper_limit || y + delta_y < @lower_limit
   end
 
 end
+start = Move.new(0,0)
+p MoveTree.new([0,0],1).out_of_bounds([0,0],[1,2])
