e1a1bfac56f80861de31d5f3d8d29ef066aabf43
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-11-09 20:00:12 -0500

    Almost done

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -2,15 +2,18 @@ require_relative "move_tree"
 class Knight
 
   def initialize(args = {})
-    @position = args[:start] || [1,0]
+    @position = args[:start] || [1, 0]
   end
 
   def move_to(destination)
     available_moves = generate_moves(@position)
     move = breadth_first_search(available_moves, destination)
+#p move
+    move_sequence = move_sequence(move)
+    format_move_sequence(move_sequence)
   end
 
-  def generate_moves(position, move_depth = 3)
+  def generate_moves(position, move_depth = 5)
     moves = MoveTree.new(position, move_depth)
     moves.generate_moves
     moves
@@ -20,12 +23,14 @@ class Knight
     queue = []
     queue.unshift(move_tree.root)
     until queue.empty?
+puts"#{queue}\n\n"
       current_node = queue.pop
       return current_node if target_square?(current_node, destination)
       current_node.children.each do |child|
         queue.unshift(child) if child
       end
     end
+    false
   end
 
   def target_square?(node, destination)
@@ -36,11 +41,18 @@ class Knight
     current_move = move
     move_list = []
     while current_move.parent
-      move_list << current_node
-      current_move = current_move.parent 
+      move_list << current_move
+      current_move = current_move.parent
     end
     move_list
   end
-  
+
+  def format_move_sequence(move_sequence)
+    move_sequence = move_sequence.map { |node| "#{node.x}, #{node.y}" }
+    move_sequence.unshift("#{@position[0]}, #{@position[1]}")
+    move_sequence.join(" -> ")
+  end
+
 end
-Knight.new.move_to([2,2])
+
+puts Knight.new.move_to([3,4])
