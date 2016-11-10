2d49799b1b1dd5cfb42e2db28bb281fa128eaf89
Christian Carey <christianrcarey@gmail.com>
2016-11-09 19:58:30 -0500

    Knight moves to any space.

--- lib/node.rb
+++ lib/node.rb
@@ -1,6 +1,6 @@
-
-
 class Node
+  attr_accessor :x, :y, :depth, :parent, :children
+
   MOVES = [ [-2,-1], [-2,1],
           [-1,2] , [-1,-2],
           [1,2]  , [1,-2],
@@ -9,28 +9,22 @@ class Node
 
   RANGE = (0..7)
 
-  def initialize(x,y, depth, parent = nil, children = nil)
+  def initialize(x,y, depth, parent = nil, children = [])
     @x = x
     @y = y
-    @depth = depth
     @parent = parent
     @children = children
-
+    @depth = depth
   end
 
   def expand
-    children = []
-    parent = node
-    depth = node.depth + 1
-
     MOVES.each do |move|
       x = @x + move[0]
       y = @y + move[1]
 
-      if RANGE.include? x && RANGE.include? y
-        @children << Node.new(x, y, @depth + 1, self, nil)
+      if RANGE.include?(x) && RANGE.include?(y)
+        @children << Node.new(x, y, @depth + 1, self)
       end
     end
   end
-
 end
