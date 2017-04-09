76186fa84b564065ad9c56b0fd56a0bd998013c4
Roy Chen <roychenlei@gmail.com>
2017-04-09 16:19:56 +0800

    first implementation of BFS

--- knight_searcher.rb
+++ knight_searcher.rb
@@ -1,12 +1,36 @@
+require_relative './move_tree'
+
 module KnightsTravails
   class KnightSearcher
+    attr_reader :tree
+
     def initialize(tree)
+      @tree = tree
     end
 
     def bfs_for(target_coords)
+      x, y = target_coords[0], target_coords[1]
+      queue = []
+      queue << tree.root
+
+      until queue.empty?
+        current_node = queue.shift
+        return current_node if current_node.x == x && current_node.y == y
+
+        current_node.children.each { |child| queue << child }
+      end
+
+      nil
     end
 
     def dfs_for(target_coords)
     end
   end
+end
+
+if $0 == __FILE__
+  include KnightsTravails
+  tree = MoveTree.new([0, 0], 1)
+  searcher = KnightSearcher.new(tree)
+  p searcher.bfs_for([2, 1])
 end
